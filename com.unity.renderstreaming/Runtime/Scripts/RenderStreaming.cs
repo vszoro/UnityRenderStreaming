6ea145c2dde616ac51f454382ef70205ba15d26e
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-10-27 15:48:35 +0900

    add initialize in player

--- com.unity.renderstreaming/Runtime/Scripts/RenderStreaming.cs
+++ com.unity.renderstreaming/Runtime/Scripts/RenderStreaming.cs
@@ -13,6 +13,8 @@ namespace Unity.RenderStreaming
 #endif
     internal static class RenderStreaming
     {
+        private static RenderStreamingSettings s_Settings;
+
         public static bool IsAutomaticEnabled()
         {
             //todo: detected by settings
@@ -22,7 +24,7 @@ namespace Unity.RenderStreaming
         public static ISignaling GetSignaling()
         {
             var schema = false ? "wss" : "ws";
-            var address = "localhost:80";
+            var address = "localhost:3000";
             var signalingInterval = 5;
             var signaling = new WebSocketSignaling(
                 $"{schema}://{address}", signalingInterval, SynchronizationContext.Current);
@@ -35,6 +37,24 @@ namespace Unity.RenderStreaming
 #if UNITY_EDITOR
             InitializeInEditor();
 #else
+            InitializeInPlayer();
+#endif
+        }
+
+        // copy from InputSystem.cs
+        ////FIXME: Unity is not calling this method if it's inside an #if block that is not
+        ////       visible to the editor; that shouldn't be the case
+        [RuntimeInitializeOnLoadMethod(loadType: RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void RunInitializeInPlayer()
+        {
+            // We're using this method just to make sure the class constructor is called
+            // so we don't need any code in here. When the engine calls this method, the
+            // class constructor will be run if it hasn't been run already.
+
+            // IL2CPP has a bug that causes the class constructor to not be run when
+            // the RuntimeInitializeOnLoadMethod is invoked. So we need an explicit check
+            // here until that is fixed (case 1014293).
+#if !UNITY_EDITOR
             InitializeInPlayer();
 #endif
         }
