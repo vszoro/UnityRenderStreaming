2bf6436edbeebb983ac18d324c5097ea2d45e33a
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-11-08 10:57:44 +0900

    update test scene

--- RenderStreaming~/Assets/Prototype/Test.cs
+++ RenderStreaming~/Assets/Prototype/Test.cs
@@ -11,6 +11,7 @@ public class Test : MonoBehaviour
     private SingleConnection singleConnection;
     private VideoStreamSender videoStreamSender;
     private VideoStreamReceiver receiveVideoViewer;
+    private AudioStreamSender audioStreamSender;
 
     [SerializeField] private RawImage remoteVideoImage;
     private void Awake()
@@ -24,45 +25,52 @@ public class Test : MonoBehaviour
         videoStreamSender.sourceTexture = remoteVideoImage.texture;
         videoStreamSender.SetTextureSize(new Vector2Int(1280, 720));
 
+        audioStreamSender = gameObject.AddComponent<AudioStreamSender>();
+        audioStreamSender.audioListener = gameObject.GetComponent<AudioListener>();
+        audioStreamSender.source = AudioStreamSource.AudioListener;
+        audioStreamSender.sourceDeviceIndex = 0;
+        audioStreamSender.autoRequestUserAuthorization = true;
+
         broadcast = gameObject.AddComponent<Broadcast>();
         broadcast.AddComponent(videoStreamSender);
+        broadcast.AddComponent(audioStreamSender);
 
-        receiveVideoViewer = gameObject.AddComponent<VideoStreamReceiver>();
-        receiveVideoViewer.OnUpdateReceiveTexture += texture => remoteVideoImage.texture = texture;
-
-        singleConnection = gameObject.AddComponent<SingleConnection>();
-        singleConnection.AddComponent(videoStreamSender);
-        singleConnection.AddComponent(receiveVideoViewer);
+        // receiveVideoViewer = gameObject.AddComponent<VideoStreamReceiver>();
+        // receiveVideoViewer.OnUpdateReceiveTexture += texture => remoteVideoImage.texture = texture;
+        //
+        // singleConnection = gameObject.AddComponent<SingleConnection>();
+        // singleConnection.AddComponent(videoStreamSender);
+        // singleConnection.AddComponent(receiveVideoViewer);
 
         renderstreaming = gameObject.AddComponent<RenderStreamingHandler>();
         renderstreaming.AddSignalingHandler(broadcast);
-        renderstreaming.settings = RenderStreaming.SignalingSettings;
+        renderstreaming.settings = RenderStreaming.GetSignalingSettings<WebSocketSignalingSettings>();
         renderstreaming.Run();
     }
 
     private void Update()
     {
-        if (Keyboard.current.aKey.isPressed)
-        {
-            if (renderstreaming.HandlerBases.Contains(singleConnection))
-            {
-                return;
-            }
-
-            renderstreaming.AddSignalingHandler(singleConnection);
-            singleConnection.CreateConnection("12345");
-        }
-
-        if (Keyboard.current.rKey.isPressed)
-        {
-            if (!renderstreaming.HandlerBases.Contains(singleConnection))
-            {
-                return;
-            }
-
-            singleConnection.DeleteConnection("12345");
-            renderstreaming.RemoveSignalingHandler(singleConnection);
-        }
+        // if (Keyboard.current.aKey.isPressed)
+        // {
+        //     if (renderstreaming.HandlerBases.Contains(singleConnection))
+        //     {
+        //         return;
+        //     }
+        //
+        //     renderstreaming.AddSignalingHandler(singleConnection);
+        //     singleConnection.CreateConnection("12345");
+        // }
+        //
+        // if (Keyboard.current.rKey.isPressed)
+        // {
+        //     if (!renderstreaming.HandlerBases.Contains(singleConnection))
+        //     {
+        //         return;
+        //     }
+        //
+        //     singleConnection.DeleteConnection("12345");
+        //     renderstreaming.RemoveSignalingHandler(singleConnection);
+        // }
 
         if (Keyboard.current.sKey.isPressed)
         {
@@ -102,6 +110,26 @@ public class Test : MonoBehaviour
             }
 
             videoStreamSender.source = VideoStreamSource.Camera;
+        }
+
+        if (Keyboard.current.mKey.isPressed)
+        {
+            if (audioStreamSender.source == AudioStreamSource.Microphone)
+            {
+                return;
+            }
+
+            audioStreamSender.source = AudioStreamSource.Microphone;
+        }
+
+        if (Keyboard.current.lKey.isPressed)
+        {
+            if (audioStreamSender.source == AudioStreamSource.AudioListener)
+            {
+                return;
+            }
+
+            audioStreamSender.source = AudioStreamSource.AudioListener;
         }
     }
 }
